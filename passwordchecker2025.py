9bdc89d97c489a1b51d9b819ea6d45f31160fe61
Saeed Khan <160650536+Skybypk@users.noreply.github.com>
2025-03-05 01:32:12 +0500

    Updated passwordchecker2025.py

--- passwordchecker2025.py
+++ passwordchecker2025.py
@@ -14,8 +14,8 @@ st.markdown("""
 <style>
     .main {text-align: center;} 
     .stTextInput {width: 60% !important; margin: auto;}
-    .stButton button {width: 50%; background-color: #blue; color: white; font-size: 18px;}
-    .stButton button:hover {background-color: #blue;}
+    .stButton button {width: 50%; background-color: blue; color: white; font-size: 18px;}
+    .stButton button:hover {background-color: darkblue;}
 </style>    
 """, unsafe_allow_html=True)
 
